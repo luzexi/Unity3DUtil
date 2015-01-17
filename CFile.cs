c2ab3bdb8423f085de51a527a9afe5cf9690f910
luzexi <jesse_luzexi@163.com>
2015-01-17 11:32:42 +0800

    add file util

---
+++ CFile.cs
@@ -0,0 +1,29 @@
+﻿
+
+using System;
+using System.IO;
+
+//  CFile.cs
+//  Author: Lu Zexi
+//  2015-01-17
+
+
+
+//file tool
+public class CFile
+{
+    //get files by dir
+    public string[] GetFiles(string dir , string param = "*")
+    {
+        if(string.IsNullOrEmpty(dir)) return null;
+
+        DirectoryInfo info = new DirectoryInfo(dir);
+        FileInfo[] fileInfos = info.GetFiles(param , SearchOption.AllDirectories);
+        string[] filenames = new string[fileInfos.Length];
+        for(int i = 0 ; i<filenames.Length ; i++)
+        {
+            filenames[i] = fileInfos[i].FullName;
+        }
+        return filenames;
+    }
+}
